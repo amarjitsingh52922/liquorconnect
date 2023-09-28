bc96f6e86fa3ab3d0d368ded97e937f3ff1728c1
Amarjit singh <amarjitaa@gmail.com>
2023-09-28 21:30:40 +0530

    updated code with hidden api

--- liquorconnect.py
+++ liquorconnect.py
@@ -3,6 +3,8 @@ import csv
 
 url = "https://connectlogisticsapiprd.azurewebsites.net/odata/Suppliers/ConnectMobile.Web.AllWithinBounds(east=-85.47607058125,west=-141.50634401875,north=60.64288417027362,south=45.01431382682483)?$top=2000&$filter=SupplierType%20eq%20%27Liquor%20Store%27"
 
+url = "https://connectlogisticsapiprd.azurewebsites.net/odata/Suppliers/?$filter=SupplierType%20eq%20%27Liquor%20Store%27"
+url = "https://connectlogisticsapiprd.azurewebsites.net/odata/Suppliers/"
 s = requests.session()
 
 
