8c0f80c73bb9a711abd67fd8e68fe7d5b5ff350d
Amarjit singh <amarjitaa@gmail.com>
2023-09-28 12:47:12 +0530

    inital project

---
+++ liquorconnect.py
@@ -0,0 +1,12 @@
+import requests
+
+url = "https://connectlogisticsapiprd.azurewebsites.net/odata/Suppliers/ConnectMobile.Web.AllWithinBounds(east=-85.47607058125,west=-141.50634401875,north=60.64288417027362,south=45.01431382682483)?$top=2000&$filter=SupplierType%20eq%20%27Liquor%20Store%27"
+
+
+def getDealerInfo():
+    pass
+
+
+getDealerInfo()
+
+print("done")
